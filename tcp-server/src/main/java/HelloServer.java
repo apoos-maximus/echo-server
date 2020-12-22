8c58e5104e22fc5c27c6ece425a23c09bda13378
Apoorv Sachan <apoorv.99.sachan@gmail.com>
2020-12-23 02:01:40 +0530

    server is able to handele sequential requests

--- tcp-server/src/main/java/HelloServer.java
+++ tcp-server/src/main/java/HelloServer.java
@@ -6,11 +6,14 @@ public class HelloServer {
     public static void main(String[] args) {
         try {
             ServerSocket ss = new ServerSocket(6666);
-            Socket s = ss.accept();
-            DataInputStream dis = new DataInputStream(s.getInputStream());
-            String str = dis.readUTF();
-            System.out.println("message = " + str);
-            ss.close();
+            while(true) {
+                System.out.println("\nlistening on tcp:6666...");
+                Socket s = ss.accept();
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                String str = dis.readUTF();
+                System.out.println("message = " + str);
+                s.close();
+            }
         } catch (Exception e) {
             System.out.println(e);
         }

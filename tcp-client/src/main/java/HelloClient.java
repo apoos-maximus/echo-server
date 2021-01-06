e752ce14cd7c95cca31f19cea284317277a9bc40
Apoorv Sachan <apoorv.99.sachan@gmail.com>
2021-01-06 23:51:57 +0530

    now session termination is determined by the client

--- tcp-client/src/main/java/HelloClient.java
+++ tcp-client/src/main/java/HelloClient.java
@@ -1,17 +1,23 @@
-import java.io.DataOutputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class HelloClient {
-    public static void main(String[] args) {
-        try {
-            Socket s = new Socket("localhost",6666);
-            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-            dos.writeUTF("Hello World !");
-            dos.flush();
-            dos.close();
-            s.close();
-        } catch(Exception e) {
-            System.out.println(e);
-        }
+    public static void main(String[] args) throws IOException {
+        Socket s = new Socket("localhost",6666);
+        DataInputStream din = new DataInputStream(s.getInputStream());
+        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str="",str2="";
+
+            while (!str.equals("stop")){
+                str = br.readLine();
+                dout.writeUTF(str);
+                dout.flush();
+                str2 = din.readUTF();
+                System.out.println("Server says : " + str2);
+            }
+        dout.close();
+        s.close();
     }
 }
--- tcp-server/src/main/java/HelloServer.java
+++ tcp-server/src/main/java/HelloServer.java
@@ -1,21 +1,27 @@
-import java.io.DataInputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.Buffer;
 
 public class HelloServer {
-    public static void main(String[] args) {
-        try {
-            ServerSocket ss = new ServerSocket(6666);
-            while(true) {
-                System.out.println("\nlistening on tcp:6666...");
-                Socket s = ss.accept();
-                DataInputStream dis = new DataInputStream(s.getInputStream());
-                String str = dis.readUTF();
-                System.out.println("message = " + str);
-                s.close();
-            }
-        } catch (Exception e) {
-            System.out.println(e);
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(6666);
+        Socket s = ss.accept();
+        DataInputStream din = new DataInputStream(s.getInputStream());
+        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str="",str2="";
+
+        while(!str.equals("stop")){
+            str = din.readUTF();
+            System.out.println("client says: " + str);
+            str2 = br.readLine();
+            dout.writeUTF(str2);
+            dout.flush();
         }
+        din.close();
+        s.close();
+        ss.close();
     }
 }
